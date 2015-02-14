1b746d047388cef0127abef3b0a3c1b50ab81825
pieroblunda <pieroblunda@gmail.com>
2015-02-14 13:43:55 -0300

    Adding toolbar menu

--- angular/directives/fullscreen.js
+++ angular/directives/fullscreen.js
@@ -1,23 +1,17 @@
 'use strict';
 
-$(document).ready(function(){
-  $('div[fullscreen]').append('<i class="fa fa-users">');
-});
-
 /**
  * @ngdoc directive
  * @name anyandgoApp.directive:fullscreen
  * @description
  * # fullscreen
  */
-angular.module('html5-canvas')
+angular.module('pbCanvas')
   .directive('fullscreen', function(){
     return{
       restrict: 'A',
       link: function postLink(scope, element, attrs){
-        $(element).click(function(){
-          console.log();
-        });
+        
       }
     };
   });
--- app.js
+++ app.js
@@ -1,3 +1,49 @@
-var app = angular.module('html5-canvas', [
+// APP MODULES ===============================================
+// congigure the modules to include on our app
+var app = angular.module('pbCanvas', [
   'evgenyneu.markdown-preview'
 ]);
+
+
+
+
+/*
+// ROUTING ===============================================
+// set our routing for this application
+// each route will pull in a different controller
+app.config(function($routeProvider) {
+
+    $routeProvider
+
+      // home page (canvas page)
+      .when('/', {
+        templateUrl: 'page-about.html',
+        controller: 'mainController'
+      })
+
+      // contact page
+      .when('/view', {
+          templateUrl: 'page-contact.html',
+          controller: 'contactController'
+      })
+  
+      // contact page
+      .when('/edit', {
+          templateUrl: 'page-contact.html',
+          controller: 'contactController'
+      })
+  
+      // about page
+      .when('/about', {
+          templateUrl: 'page-about.html',
+          controller: 'aboutController'
+      });
+
+});
+
+// CONTROLLERS ============================================
+// home page controller
+app.controller('mainController', function($scope) {
+    $scope.pageClass = 'page-home';
+});
+*/
