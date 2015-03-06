632969fc21e69414b26afed53d806847fc350642
pieroblunda <pieroblunda@gmail.com>
2015-03-06 18:08:33 -0300

    Test concept

--- app.js
+++ app.js
@@ -59,38 +59,54 @@ app.config(function($routeProvider) {
 // home page controller
 
 app.controller("mainController", function($scope, $routeParams, $location) {
-  var localData = JSON.parse(localStorage.getItem("pbBMC"));
-  if(localData == undefined) {
-    $.getJSON( "template.json", function( data ) { //Get template DB
-      localStorage.setItem("pbBMC", JSON.stringify(data));
+
+  //LocalStorage
+  var localdata = '';
+  if(chrome.app.window){
+    chrome.storage.local.set({key: 'Piero'}, function() {console.log('Settings saved');});
+    chrome.storage.local.get('key', function(localdata){
+      $('#probando').html('ChromeApp '+localdata.key);
     });
+    
+  }else{
+    dataobject = JSON.stringify({key: 'Piero'});
+    localStorage.setItem('pbStorage', dataobject);
+    localdata = JSON.parse(localStorage.getItem('pbStorage'));
+    $('#probando').html('WebApp '+localdata.key);
   }
-  $scope.localdata = localData;
+  
+//  var localData = JSON.parse(localStorage.getItem("pbBMC"));
+//  if(localData == undefined) {
+//    $.getJSON( "template.json", function( data ) { //Get template DB
+//      localStorage.setItem("pbBMC", JSON.stringify(data));
+//    });
+//  }
+//  $scope.localdata = localData;
 });
 
 app.controller("viewController", function($scope, $routeParams, $location) {
-  $scope.params = $routeParams;
-  
-  //Read localstorage
-  var localData = JSON.parse(localStorage.getItem("pbBMC"));
-  if(localData !== undefined)
-    $scope.textMD = localData[$routeParams.boxname];
-  
-  //Read boxes information
-  $.getJSON( "boxes.json", function( data ) {
-    $scope.box = data[$routeParams.boxname];
-    $scope.boxes = data;
-    $scope.$apply();
-  });
+//  $scope.params = $routeParams;
+//  
+//  //Read localstorage
+//  var localData = JSON.parse(localStorage.getItem("pbBMC"));
+//  if(localData !== undefined)
+//    $scope.textMD = localData[$routeParams.boxname];
+//  
+//  //Read boxes information
+//  $.getJSON( "boxes.json", function( data ) {
+//    $scope.box = data[$routeParams.boxname];
+//    $scope.boxes = data;
+//    $scope.$apply();
+//  });
   
   //Write localstorage
-  if( $location.path().match('\/edit\/') ) {
-    setInterval(
-      function () {
-        localData[$routeParams.boxname] = $('textarea').val();
-        localStorage.setItem("pbBMC", JSON.stringify(localData));
-      },
-      3000
-    );
-  }
+//  if( $location.path().match('\/edit\/') ) {
+//    setInterval(
+//      function () {
+//        localData[$routeParams.boxname] = $('textarea').val();
+//        localStorage.setItem("pbBMC", JSON.stringify(localData));
+//      },
+//      3000
+//    );
+//  }
 });
